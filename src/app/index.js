be1b3a6b06944a6f6cdf7d0f023617adc61f3fa4
Disorder <disorrder@gmail.com>
2018-06-03 23:34:46 +0300

    add hotkeys proto

--- src/app/index.js
+++ src/app/index.js
@@ -20,3 +20,24 @@ $(() => {
     });
     window.app = app;
 });
+
+// Hotkeys
+// TODO: wrap in file
+const electron = window.require('electron');
+$(document).on('keydown.hotkey', (e) => {
+    var combo = [e.ctrlKey&&'ctrl',  e.altKey&&'alt',  e.shiftKey&&'shift', e.originalEvent.code].filter((v) => v);
+    var comboString = combo.join('+');
+    // console.log('Hotkey:', comboString, e.keyCode);
+    if (hotkeys[comboString]) hotkeys[comboString]();
+
+    // if (comboString === 'ctrl+shift+KeyU') {
+    //     // app.emit('openDevTools');
+    // }
+});
+
+var hotkeys = {
+    "ctrl+shift+KeyK"() {
+        electron.remote.webContents.getAllWebContents()[0].toggleDevTools(); // wow wtf why
+        // app.emit('openDevTools');
+    },
+}
--- src/lib/worker.js
+++ src/lib/worker.js
@@ -1,9 +1,9 @@
 require('./index');
 
 process.on('message', (message) => {
-    console.log('MESSAGE', message);
     if (message === 'exit') return process.exit();
     if (typeof message !== 'object') return;
+    console.log('MESSAGE', message);
     if (message.type === 'require') return require(message.module); //? need?
 });
 
